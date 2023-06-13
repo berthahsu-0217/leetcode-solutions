0410c65f4c9576166173274ba7491ade7bd8af9c
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-13 16:24:34 +0800

    Time: 59 ms (69.13%), Space: 16.4 MB (11.01%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        vals = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        
+        ans = ""
+        
+        for r, n in zip(roman, vals):
+            ans += r*(num//n)
+            num %= n
+            
+        return ans
+        
+            
+            
+            
+            
+        
+        
+        
+        
+        
