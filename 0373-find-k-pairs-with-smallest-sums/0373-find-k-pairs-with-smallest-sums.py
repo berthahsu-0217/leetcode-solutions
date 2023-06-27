35c5047491feb2abd354de924137e32dd52ee8d9
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-27 11:30:20 +0800

    Time: 1183 ms (30.02%), Space: 43.3 MB (5.35%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,8 +1,11 @@
 import heapq as hq
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        
+        return self.solution1(nums1, nums2, k)
+    
+    def solution1(self, nums1, nums2, k):
         """
+        Solution 1
         i
         1 4 5 6 9
         2 2 3 4 7
@@ -11,8 +14,10 @@ class Solution:
         i = 0 [[1,2],[1,2],[1,3],[1,4],[1,7]]
         i = 1 [[4,2],[4,2],[4,3],[4,4],[4,7]]
         i = 2 [[5,2],[5,2],...]
+        i = 3 ...
+        i = 4 ...
         
-        => find k elements in m sorted lists of length n
+        => find first k smallest elements in m sorted lists of length n
         """
         pairs = []
         m, n = len(nums1), len(nums2)
