10964aab0b6fff6405bb597cb5e9d934e9e897a4
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-27 12:12:52 +0800

    Time: 1129 ms (55.55%), Space: 35.3 MB (69.98%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,14 +1,14 @@
 import heapq as hq
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        return self.solution1(nums1, nums2, k)
+        #return self.solution1(nums1, nums2, k)
+        return self.solution2(nums1, nums2, k)
     
     def solution1(self, nums1, nums2, k):
         """
-        Solution 1
         i
         1 4 5 6 9
-        2 2 3 4 7
+        2 3 4 5 7
         j
         
         i = 0 [[1,2],[1,2],[1,3],[1,4],[1,7]]
@@ -34,5 +34,47 @@ class Solution:
                 hq.heappush(min_heap, (nums1[i]+nums2[j+1], i, j+1))
                 
         return pairs
+    
+    def solution2(self, nums1, nums2, k):
+        
+        """
+        0 1 2 3 4
+        i
+        1 4 5 6 9
+        2 2 3 4 7
+        j
+        
+        [1,2] => [1,2] or [4,2]
+        [1,2] => [1,3] or [4,2] or [4,2]
+        [1,3] => [1,4] or [4,3] or [4,2] or [4,2]
+        [1,4] => [1,7] or [4,4] or [4,3] or [4,4] or [4,4]
+        
+        => next to compare is nums1[i+1]+nums2[j] or nums1[i]+num2[j+1]
+        """
+        pairs = []
+        m, n = len(nums1), len(nums2)
+        
+        min_heap = [(nums1[0]+nums2[0], 0, 0)]
+        visited = {(0, 0)}
+            
+        #O(klogk)
+        for t in range(min(k, m*n)):
+            _, i, j = hq.heappop(min_heap)
+            pairs.append([nums1[i], nums2[j]])
+            
+            if i+1 < m and (i+1, j) not in visited:
+                hq.heappush(min_heap, (nums1[i+1]+nums2[j], i+1, j))
+                visited.add((i+1, j))
+                
+            if j+1 < n and (i, j+1) not in visited:
+                hq.heappush(min_heap, (nums1[i]+nums2[j+1], i, j+1))
+                visited.add((i, j+1))
+        
+        return pairs
+            
+            
+        
+        
+    
                 
         
