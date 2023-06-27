e6b1bcb1b16abf5acb491398c659f5a8831125cf
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-27 16:23:01 +0800

    Time: 77 ms (56.10%), Space: 16.5 MB (22.44%) - LeetHub

--- 0330-patching-array/0330-patching-array.py
+++ 0330-patching-array/0330-patching-array.py
@@ -1,7 +1,14 @@
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
         
-        ans = 0
+        """
+        [1,3]
+        
+        Each time adding an extra element nums[i], check to see if max_sum+1 can be reached
+        if not: pad max_sum+1 to array => max_num += max_num+1
+        if can: i += 1
+        """
+        pads = 0
         max_num = 0
         i, lens = 0, len(nums)
         
@@ -11,9 +18,9 @@ class Solution:
                 i += 1
             else:
                 max_num += max_num+1
-                ans += 1
-        
-        return ans
+                pads += 1  #pad max_num+1
+                
+        return pads
             
                 
             
