a08d8cd4981136d3672bb774a3cb53900f6a81ca
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-28 10:19:13 +0800

    Time: 477 ms (68.91%), Space: 39.3 MB (25.69%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        
+        
+        n = len(nums)
+        
+        num_and_cost = [(num, c) for num, c in zip(nums, cost)]
+        num_and_cost.sort()
+        
+        prefix_cost = [0]*n
+        prefix_cost[0] = num_and_cost[0][1]
+        for i in range(1, n):
+            prefix_cost[i] = prefix_cost[i-1]+num_and_cost[i][1]
+        
+        #Make elements equal to nums[0]
+        total_cost = 0
+        for i in range(1, n):
+            total_cost += (num_and_cost[i][0]-num_and_cost[0][0])*num_and_cost[i][1]
+        
+        #Make elements equal to nums[i]
+        ans = total_cost
+        for i in range(1, n):
+            gap = num_and_cost[i][0]-num_and_cost[i-1][0]
+            inc = prefix_cost[i-1] #additional costs for elements larger than nums[i]
+            dec = prefix_cost[n-1]-prefix_cost[i-1] #decreased costs for elements larger than nums[i]
+            total_cost += gap*(inc-dec)
+            ans = min(ans, total_cost)
+        
+        return ans
+            
+        
+        
+        
+            
+        
+            
+            
