2bb93b56db141a2e224cbd7c6e1e19d83a9c119c
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-30 21:12:52 +0800

    Time: 3276 ms (34.74%), Space: 17.1 MB (15.26%) - LeetHub

--- 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.py
+++ 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.py
@@ -1,38 +1,4 @@
-class Solution:
-    def bfs(self, node, answer, graph):
-        seen = set([node])
-        todo = deque([node])
-        depts = defaultdict(int)
-        depts[node] = 0
-        parents = defaultdict(int)
-        parents[node] = -1
-        while todo:
-            cur = todo.popleft()
-            for neighbor in graph[cur]:
-                if neighbor not in seen:
-                    todo.append(neighbor)
-                    seen.add(neighbor)
-                    depts[neighbor] = depts[cur] + 1
-                    parents[neighbor] = cur
-                elif neighbor != parents[cur]:
-                    answer[0] = min(answer[0], depts[neighbor] + depts[cur] + 1)
-                
-    def findShortestCycle(self, n: int, edges: List[List[int]]) -> int:
-        graph = [ [] for _ in range(n) ]
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
-        
-        answer = [1001]
-        for node in range(n):
-            self.bfs(node, answer, graph)
-        
-        if answer[0] == 1001:
-            answer[0] = -1
-        
-        return answer[0]
-"""
-import queue
+from collections import deque
 
 class Solution:
     def findShortestCycle(self, n: int, edges: List[List[int]]) -> int:
@@ -41,30 +7,24 @@ class Solution:
 
         def bfs(start):
             
-            min_cycle = float("inf")
-            dist = [float("inf")]*n
-            dist[start] = 0
-            parent = [-1]*n
-            q = queue.Queue()
-            q.put(start)
+            min_cycle = 1001
+            dist = {start:0}
+            parent = {start:-1}
+            q = deque([start])
             
-            V = 0
-            E = 0
-            while not q.empty():
-                node = q.get()
+            while q:
+                node = q.popleft()
                 #visited.add(node)
-                V += 1
-                E += len(graph[node])
                 for child in graph[node]:
                     #unvisited
-                    if dist[child] == float("inf"):
+                    if child not in dist:
                         dist[child] = dist[node]+1
                         parent[child] = node
-                        q.put(child)
+                        q.append(child)
                     #visited before by other paths rather than the current parent
                     elif parent[node] != child and parent[child] != node:
                         #print(V, E)
-                        return dist[node]+dist[child]+1
+                        min_cycle = min(min_cycle, dist[node]+dist[child]+1)
             
             return min_cycle
         
@@ -74,15 +34,12 @@ class Solution:
             graph[v].add(u)
         
         #visited = set()
-        min_cycle = float("inf")
+        min_cycle = 1001
         for i in range(n):
             #if i not in visited:
             min_cycle = min(min_cycle, bfs(i))
-            if min_cycle == 3:
-                return 3
         
-        if min_cycle == float("inf"):
+        if min_cycle == 1001:
             return -1
         
         return min_cycle
-"""
