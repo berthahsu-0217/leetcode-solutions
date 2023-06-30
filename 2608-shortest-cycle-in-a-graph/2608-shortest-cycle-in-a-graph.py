0b8b89a050597b57c5aa551caa586112ef947472
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-06-30 21:14:47 +0800

    Time: 3087 ms (42.11%), Space: 16.9 MB (65.79%) - LeetHub

--- 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.py
+++ 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.py
@@ -3,8 +3,7 @@ from collections import deque
 class Solution:
     def findShortestCycle(self, n: int, edges: List[List[int]]) -> int:
 
-        #A cycle can be seen as two shortest paths (with different parents) meet at a point
-
+        #A cycle can be seen as two shortest paths (with different parents) meet at a node
         def bfs(start):
             
             min_cycle = 1001
@@ -23,7 +22,6 @@ class Solution:
                         q.append(child)
                     #visited before by other paths rather than the current parent
                     elif parent[node] != child and parent[child] != node:
-                        #print(V, E)
                         min_cycle = min(min_cycle, dist[node]+dist[child]+1)
             
             return min_cycle
@@ -33,10 +31,8 @@ class Solution:
             graph[u].add(v)
             graph[v].add(u)
         
-        #visited = set()
         min_cycle = 1001
         for i in range(n):
-            #if i not in visited:
             min_cycle = min(min_cycle, bfs(i))
         
         if min_cycle == 1001:
