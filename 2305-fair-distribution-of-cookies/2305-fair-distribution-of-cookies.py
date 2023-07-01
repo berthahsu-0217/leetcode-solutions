05ac71a3731be24a9da881049619dd0fab886a2d
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-07-01 10:47:47 +0800

    Time: 1004 ms (52.99%), Space: 16.3 MB (89.55%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -2,26 +2,28 @@ class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         
         """
-        k <= len(cookies) => each child receives at least one bag of cookies
+        k <= len(cookies) => each child receives at least one bag of cookies (pigeon hole rule)
         early stopping: compare number of children without cookies and the remaining bags to distribute to
         """
         
         def backtrack(i, n, no_cookies):
             nonlocal ans
             
-            if (n-i) < no_cookies:
+            #early stopping
+            if (n-i) < no_cookies: #if number of children without cookies are greater than the remaining bags
                 return
+            
+            #base case
             if i >= n:
                 if no_cookies == 0:
                     ans = min(ans, max(children))
                 return
             
             for j in range(k):
-                flag = (children[j] == 0)
+                x = int(children[j] == 0)
                 children[j] += cookies[i]
-                backtrack(i+1, n, no_cookies-int(flag))
+                backtrack(i+1, n, no_cookies-x)
                 children[j] -= cookies[i]
-                
         
         ans = float("inf")
         children = [0]*k
