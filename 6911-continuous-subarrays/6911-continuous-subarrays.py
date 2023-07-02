f75d5981f15f7161ef0eabd5f1a9141cfad3e003
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-07-02 13:25:43 +0800

    Time: 960 ms (54.55%), Space: 26.7 MB (100.00%) - LeetHub

---
+++ 6911-continuous-subarrays/6911-continuous-subarrays.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        
+        max_deque = deque([]) #stricly decreasing
+        min_deque = deque([]) #stricly increasing
+        
+        ans = 0
+        n = len(nums)
+        l = r = 0
+        
+        while r < n:
+            while max_deque and max_deque[-1][0] <= nums[r]:
+                max_deque.pop()
+            max_deque.append([nums[r], r])
+            while min_deque and min_deque[-1][0] >= nums[r]:
+                min_deque.pop()
+            min_deque.append([nums[r], r])
+            
+            while (max_deque[0][0]-min_deque[0][0]) > 2:
+                l += 1
+                while max_deque and max_deque[0][-1] < l:
+                    max_deque.popleft()
+                while min_deque and min_deque[0][-1] < l:
+                    min_deque.popleft()
+            ans += r-l+1
+            r += 1
+            
+        return ans
