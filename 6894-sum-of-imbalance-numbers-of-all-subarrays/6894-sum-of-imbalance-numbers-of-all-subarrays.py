98e8af64ec97301993fa71d3cc3242273cff9439
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-07-02 16:38:20 +0800

    Time: 624 ms (100.00%), Space: 16.6 MB (75.00%) - LeetHub

---
+++ 6894-sum-of-imbalance-numbers-of-all-subarrays/6894-sum-of-imbalance-numbers-of-all-subarrays.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sumImbalanceNumbers(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        ans = 0
+        
+        for i in range(n):
+            vals = set()
+            imbalances = 0
+            for j in range(i, n):
+                #nums[j] is already in the set, does not create any more gap
+                if nums[j] in vals:
+                    pass
+                #original gap: nums[j-1], nums[j+1] => by adding nums[j] the gap is gone
+                elif (nums[j]-1 in vals) and (nums[j]+1 in vals):
+                    imbalances -= 1
+                #if vals already has some elements, adding nums[j] will add an additional gap
+                elif (nums[j]-1 not in vals) and (nums[j]+1 not in vals) and vals:
+                    imbalances += 1
+                vals.add(nums[j])
+                ans += imbalances
+        
+        return ans
+                    
