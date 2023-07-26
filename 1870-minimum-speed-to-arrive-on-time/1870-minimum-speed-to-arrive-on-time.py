f8a27acede745c6feed787cc5cfdc8f7d0a7633e
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-07-26 15:58:58 +0800

    Time: 3706 ms (34.47%), Space: 30.8 MB (56.98%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,28 @@
+import math
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        
+        """
+        hour >= ceil(d1/speed)+ceil(d2/speed)+ceil(d3/speed)+...
+        """
+        def verify(speed):
+            if speed == 0:
+                return False
+            h = 0
+            for i in range(n-1):
+                h += math.ceil(dist[i]/speed)
+            h += dist[-1]/speed
+            return h <= hour
+            
+        n = len(dist)
+        l, r = 0, 10000001
+        
+        while l < r:
+            m = (l+r)//2
+            #print(m, verify(m))
+            if verify(m):
+                r = m
+            else:
+                l = m+1
+        return -1 if l == 10000001 else l
+        
