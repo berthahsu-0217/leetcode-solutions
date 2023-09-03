32e71f65e4aa3cb79855a181b6465b0120d353bb
berthahsu-0217 <115740326+berthahsu-0217@users.noreply.github.com>
2023-09-03 13:11:25 +0800

    Time: 406 ms (100.00%), Space: 16.2 MB (100.00%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.py
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.py
@@ -1,26 +1,13 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         
-        def backtrack(arr, val):
-            
-            if val > high:
-                return 0
-            
-            ways = 0
-            
-            if low <= val <= high:
-                if len(arr) == 2 and arr[0] == arr[1]:
-                    ways += 1
-                if len(arr) == 4 and arr[0]+arr[1] == arr[2]+arr[3]:
-                    ways += 1
-
-            for i in range(10):
-                if len(arr) == 0 and i == 0:
-                    continue
-                arr.append(i)
-                ways += backtrack(arr, val*10+i)
-                arr.pop()
-                
-            return ways
-
-        return backtrack([], 0)
+        ans = 0
+        
+        for i in range(low, high+1):
+            s = str(i)
+            if len(s) == 2 and int(s[0])==int(s[1]):
+                ans += 1
+            if len(s) == 4 and int(s[0])+int(s[1])==int(s[2])+int(s[3]):
+                ans += 1
+        
+        return ans
